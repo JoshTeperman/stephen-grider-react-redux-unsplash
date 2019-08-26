078267bf9b422335d8e5cc178919d77eef8728c2
Josh Teperman <joshteperman@gmail.com>
2019-08-26 12:21:19 +1000

    css: automatically adjusts grid view to match image height

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './ImageList.css';
-import ImageCard from './ImageCard'
+import ImageCard from './ImageCard';
 
-const ImageList = (props) => {
-  const images = props.images.map(({ description, id, urls }) => {
-    return <img alt={description} key={id} src={urls.regular} />
+const ImageList = (props) => { 
+  const images = props.images.map(image => {
+    return <ImageCard key={image.id} image={image}/>
+    // const images = props.images.map(({ description, id, urls }) => {
+    // return <img alt={description} key={id} src={urls.regular} />
     // return <ImageCard key={image.id} image={image} />;
   });
 
